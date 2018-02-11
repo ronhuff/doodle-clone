cd08b589b3b83b32d24282d8a5fa896d00d0f1ae
Ron Huff <ron.huff@ku.edu>
2018-02-11 10:37:15 -0600

    Timeslots show proper times and push creater to timeslots.attendees by default.

--- driver.js
+++ driver.js
@@ -55,7 +55,7 @@ var driver =
         console.log("Preparing to enter dupPersons(html:58) *DBG");
 
         //This block ensures that we cannot have any duplicate persons.
-        if(!dupPersons)
+        if(!dupPersons())
         {
             var person = {firstname:"", lastname:creator, isAttendee:true};
         }
@@ -70,16 +70,31 @@ var driver =
                 }
             }
 
-            this.meetings.push(meeting);
-            this.meetings[this.numMeetings].attendees.push(person);
+
 
 
         }
+        this.meetings.push(meeting);
+        this.meetings[this.numMeetings].attendees.push(person);
         for(i = 0; i < this.meetings[this.numMeetings].numTimeSlots; i++)
         {
+            console.log(t1 + " " + t2 + " " + t3 + " " + t4);
             console.log("ev_demo.html:119 *DBG");
             //if(( (t1 != t3) && t2 !>))
             var testBool = (t2 + 20 == 0);
+            console.log(testBool);
+
+            if(i > 0)
+            {
+                if(t2 == 0)
+                {
+                    t1++;
+                }
+            }
+            if(t2 == 0)
+            {
+                t2 = "00";
+            }
             var timeslot =
             {
 
@@ -89,16 +104,31 @@ var driver =
                     t1++,//This accounts for starting at 4:40 and ending at 5:00
                 }*/
 
-                etime: testBool ? (String(t1++) + ':' + String(t2)) : (String(t1) + ':' + String(t2 + 20)),
+                etime:"",
                 startHr:t1,
-                endHr:t3,
-                endMin:t4,
+                startMin:t2,
+                endHr:0,
+                endMin:0,
                 attendees:[],//if person is in this array, they are available for this timeslot in the meeting
                 numAttendees:1
             };
-
+            t2 = Number(t2) + 20;
+            if(t2 >= 60)
+            {
+                t2 = 0;
+                timeslot.etime = String(t1) + ':' + "00";
+                timeslot.endHr = t1;
+                timeslot.endMin = t2;
+            }
+            else
+            {
+                timeslot.etime = String(t1) + ':' + String(t2);
+                timeslot.endHr = t1;
+                timeslot.endMin = t2;
+            }
             console.log("pushing timeslots ev_demo.html:137 *DBG");
             timeslot.attendees.push(person);
+            if(timeslot.startMin == "00") timeslot.startMin = 0;
             meeting.timeSlots.push(timeslot);
 
         }
