53695ec3196a7a76b1b77e43b0a8777bb202ecb2
Diego Garcia <diego.alejandro.garcia09@gmail.com>
2018-02-11 20:45:52 -0600

    deleted the code that was commented out in the newDataFile.js

--- newDataFile.js
+++ newDataFile.js
@@ -112,58 +112,6 @@ function enteringEvent(popTimeSlots){
   }
 }
 
-/*function tableCreate() {
-    var body = document.getElementsByTagName('body')[0];
-    var tbl = document.createElement('table');
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
-    var tbdy = document.createElement('tbody');
-    for (var i = 0; i <= 1; i++) {
-        var tr = document.createElement('tr');
-        for (var j = 0; j < 3; j++) {
-            if (i === 0 && j === 0)
-            {
-                var td = document.createElement('td');
-                td.appendChild(document.createTextNode("Event Name"))
-                tr.appendChild(td)
-            }
-            else if (i === 0 && j === 1)
-            {
-                var td = document.createElement('td');
-                td.appendChild(document.createTextNode("Start Time"))
-                tr.appendChild(td)
-            }
-            else if (i === 0 && j === 2)
-            {
-              var td = document.createElement('td');
-              td.appendChild(document.createTextNode("End Time"))
-              tr.appendChild(td)
-            }
-            else if  (i === 1 && j === 0)
-            {
-              var td = document.createElement('td');
-              td.appendChild(document.createTextNode(nEvent))
-              tr.appendChild(td)
-            }
-            else if (i === 1 && j === 1)
-            {
-              var td = document.createElement('td');
-              td.appendChild(document.createTextNode(sTime))
-              tr.appendChild(td)
-            }
-            else
-            {
-              var td = document.createElement('td');
-              td.appendChild(document.createTextNode(eTime))
-              tr.appendChild(td)
-            }
-        }
-        tbdy.appendChild(tr);
-    }
-    tbl.appendChild(tbdy);
-    body.appendChild(tbl)
-}*/
-
 function storeData(){
   localStorage.setItem("objectString", JSON.stringify(events));
 }
